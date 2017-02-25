91b06b0bbd1814d3553b9faafe46a1c200ee60d0
Gautham <thabeatsz@gmail.com>
2017-02-26 04:33:10 +0530

    terminal setup

---
+++ terminal.sh
@@ -0,0 +1,21 @@
+#How do i tells you how to do things
+pip install howdoii
+#bro is a command to give you examples of a command
+gem install bropages
+#htop  for showing process
+sudo apt-get install htop
+#tmux for terminal multiplexing : press ctrl + b after that , press % and " to split h,v
+sudo apt-get install tmux
+
+#for fun
+
+#sl 
+sudo apt-get install sl
+#lolcat for fun it gives rainbowcolors
+gem install lolcat
+#fortune to get quotes random
+sudo apt-get install fortune
+#cowsay for awesomeness try cowsay -f tux something
+sudo apt-get install cowsay
+
+
